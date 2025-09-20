1960fe4e425f11f6410d7b6b37d4fb28e4f7cb4b
rajdeep Majumdar <amazumder781@gmail.com>
2025-09-20 23:23:36 +0530

    add report-form

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import ReportForm from "../pages/ReportForm";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/report" element={<ReportForm />} />
       </Routes>
     </BrowserRouter>
   );
