14efcea5d10d40c48ee7507b5d2f0b7cd403e9b1
Supratik Ray <raySupratik8@gmail.com>
2025-09-20 17:57:13 +0530

    Added issues table Api functions

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,17 @@
 //Riya ekahne paste korish tor code ar components gulo component folder e
+import { useEffect } from "react";
+import { fetchIssues } from "../supabase/api/issues";
 
 const HomePage = () => {
+  useEffect(() => {
+    async function getAllIssues() {
+      const result = await fetchIssues({ category_id: 1 });
+      if (!result.success) return console.log(result.error);
+      console.log(result.data);
+    }
+
+    getAllIssues();
+  }, []);
   return <div>HomePage💩</div>;
 };
 
