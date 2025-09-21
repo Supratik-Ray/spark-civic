aa0c391f86e72c2fbcb71ab7e5b7d503e2ca75af
Purbasha Goswami <purbashagoswami2@gmail.com>
2025-09-21 14:49:24 +0530

    Added Admin Page

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import AdminPage from "../pages/AdminPage";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   );
